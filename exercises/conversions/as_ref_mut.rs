0a9567a0ed236df5819650d464ec316cd9c83c9a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-08-09 20:05:12 -0400

    feat: complete (with some interesting type algebra for AsMut)

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -3,25 +3,42 @@
 // and https://doc.rust-lang.org/std/convert/trait.AsMut.html, respectively.
 // Execute `rustlings hint as_ref_mut` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 // Obtain the number of bytes (not characters) in the given argument.
 // TODO: Add the AsRef trait appropriately as a trait bound.
-fn byte_counter<T>(arg: T) -> usize {
+fn byte_counter<T>(arg: T) -> usize
+where
+    T: AsRef<str>,
+{
     arg.as_ref().as_bytes().len()
 }
 
 // Obtain the number of characters (not bytes) in the given argument.
 // TODO: Add the AsRef trait appropriately as a trait bound.
-fn char_counter<T>(arg: T) -> usize {
+fn char_counter<T>(arg: T) -> usize
+where
+    T: AsRef<str>,
+{
     arg.as_ref().chars().count()
 }
 
 // Squares a number using as_mut().
 // TODO: Add the appropriate trait bound.
-fn num_sq<T>(arg: &mut T) {
+fn num_sq<T, P>(arg: &mut T)
+where
+    T: AsMut<P>,
+    // P: std::ops::Mul<Output = P> + Clone,
+    P: std::ops::Mul<Output = P> + Copy,
+{
     // TODO: Implement the function body.
-    ???
+
+    // // if only implementing Clone & not Copy
+    // let val = (*arg.as_mut()).clone();
+    // *arg.as_mut() = val.clone() * val
+
+    // if implementing Copy
+    let val = *arg.as_mut();
+    *arg.as_mut() = val * val
 }
 
 #[cfg(test)]
